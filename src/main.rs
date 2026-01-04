ccadb835930237c51952a5f5b51cb578e4972a41
Crazygiscool <crazygiscool@proton.me>
2026-01-03 21:49:20 -0500

    added simple exit command to quit the program

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ use std::io::{self, Write};
 
 fn main() {
     let mut command = String::new();
-    
+    let commands = ["exit"];
     loop{
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        println!("{}: command not found", command.trim());
+        if command.trim().is_empty() {
+            continue;
+        }else if commands.contains(&command.trim()){
+            break;
+        }else{
+            println!("{}: command not found", command.trim());
+        }
         command.clear();
     }
-
 }
