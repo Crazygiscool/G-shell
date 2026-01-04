3ca448c97c9a1da8f2a61a02ecd31a2f909c7564
Crazygiscool <crazygiscool@proton.me>
2026-01-04 00:28:45 -0500

    make sure to use only directory

--- src/main.rs
+++ src/main.rs
@@ -28,21 +28,21 @@ fn main() {
         if directory.is_empty() {
             if let Some(home) = std::env::var_os("HOME") {
                 std::env::set_current_dir(home).unwrap_or_else(|e| {
-                    eprintln!("cd: {}: No such file or directory", e);
+                    eprintln!("cd: {}: No such file or directory", directory);
                 });
             }
             return;
         }else if directory == ".." {
             if let Some(parent) = std::env::current_dir().unwrap().parent() {
                 std::env::set_current_dir(parent).unwrap_or_else(|e| {
-                    eprintln!("cd: {}: No such file or directory", e);
+                    eprintln!("cd: {}: No such file or directory", directory);
                 });
             }
             return;
         }
         match std::env::set_current_dir(directory) {
             Ok(_) => (),
-            Err(e) => eprintln!("cd: {}: No such file or directory", e),
+            Err(e) => eprintln!("cd: {}: No such file or directory", directory),
         }
     }
 
