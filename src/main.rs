ad9ea733a35feec3b08b665a46b6f4e6dd7de92a
Crazygiscool <crazygiscool@proton.me>
2026-01-03 22:53:07 -0500

    re wrote the command system to match  with name and added echo command

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    let mut command = String::new();
-    let commands = ["exit"];
-    loop{
+    let mut command: String = String::new();
+
+    fn echo(string: &str) {
+        let echo_content = string[5..].trim();
+        println!("{}", echo_content);
+    }
+
+    loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut command).unwrap();
-        if command.trim().is_empty() {
+        
+        let cmd = command.trim();
+        
+        if cmd.is_empty() {
+            command.clear();
             continue;
-        }else if commands.contains(&command.trim()){
-            break;
-        }else{
-            println!("{}: command not found", command.trim());
         }
+        
+        let command_name = cmd.split_whitespace().next().unwrap_or("");
+        
+        match command_name {
+            "exit" => break,
+            "echo" => echo(&command),
+            _ => println!("{}: command not found", command_name),
+        }
+
         command.clear();
     }
 }
