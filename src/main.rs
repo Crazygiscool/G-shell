adc2fed03695537241d71d92b00c15e681c8d54d
Crazygiscool <crazygiscool@proton.me>
2026-01-04 00:24:29 -0500

    chnaged project name and added cd command

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use std::os::unix::process::CommandExt;
 
 fn main() {
     let mut command: String = String::new();
-    let regix: &[[&str; 2]; 4] = &[
+    let regix: &[[&str; 2]; 5] = &[
         ["echo", "builtin"],
         ["type", "builtin"],
         ["exit", "builtin"],
         ["pwd", "builtin"],
+        ["cd", "builtin"],
     ];
 
     fn echo(string: &str) {
@@ -23,7 +24,29 @@ fn main() {
         }
     }
 
-    fn r#type(command: &str, regix: &[[&str; 2]; 4]) {
+    fn cd(directory: &str) {
+        if directory.is_empty() {
+            if let Some(home) = std::env::var_os("HOME") {
+                std::env::set_current_dir(home).unwrap_or_else(|e| {
+                    eprintln!("cd: {}: No such file or directory", e);
+                });
+            }
+            return;
+        }else if directory == ".." {
+            if let Some(parent) = std::env::current_dir().unwrap().parent() {
+                std::env::set_current_dir(parent).unwrap_or_else(|e| {
+                    eprintln!("cd: {}: No such file or directory", e);
+                });
+            }
+            return;
+        }
+        match std::env::set_current_dir(directory) {
+            Ok(_) => (),
+            Err(e) => eprintln!("cd: {}: No such file or directory", e),
+        }
+    }
+
+    fn r#type(command: &str, regix: &[[&str; 2]; 5]) { // change this if u changed the regix size
         if let Some(entry) = regix.iter().find(|cmd| cmd[0] == command) {
             println!("{} is a shell {}", entry[0], entry[1]);
         }else if find_executable_in_path(command).is_some() {
@@ -73,6 +96,7 @@ fn main() {
             "echo" => echo(&content),
             "type" => r#type(&content, regix),
             "pwd" => pwd(),
+            "cd" => cd(&content),
             _ => execute(cmd, &args),
         }
 
