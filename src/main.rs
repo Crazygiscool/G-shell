1497e7a86a5b533f48d16f96e78281f4226ce5f8
Crazygiscool <crazygiscool@proton.me>
2026-01-04 00:30:36 -0500

    make sure temporary error vars are working

--- src/main.rs
+++ src/main.rs
@@ -27,14 +27,14 @@ fn main() {
     fn cd(directory: &str) {
         if directory.is_empty() {
             if let Some(home) = std::env::var_os("HOME") {
-                std::env::set_current_dir(home).unwrap_or_else(|e| {
+                std::env::set_current_dir(home).unwrap_or_else(|_e| {
                     eprintln!("cd: {}: No such file or directory", directory);
                 });
             }
             return;
         }else if directory == ".." {
             if let Some(parent) = std::env::current_dir().unwrap().parent() {
-                std::env::set_current_dir(parent).unwrap_or_else(|e| {
+                std::env::set_current_dir(parent).unwrap_or_else(|_e| {
                     eprintln!("cd: {}: No such file or directory", directory);
                 });
             }
@@ -42,7 +42,7 @@ fn main() {
         }
         match std::env::set_current_dir(directory) {
             Ok(_) => (),
-            Err(e) => eprintln!("cd: {}: No such file or directory", directory),
+            Err(_e) => eprintln!("cd: {}: No such file or directory", directory),
         }
     }
 
